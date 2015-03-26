a6c5205ecb09df523d1965097fcbba943c4cd08e
4udesenko <nikita@joylia.com>
2015-03-26 18:52:07 +0300

    Update image.php

--- Lesson_7/pages/image.php
+++ Lesson_7/pages/image.php
@@ -1,13 +1,15 @@
+<h1>Изображение</h1>
 <div class="image">
-    <img src="./images/<?php echo $image['image']; ?>">
-    <h4>Название: <?= $image['tittle'] ?></h4>
-    Дата добавления: <?= $image['date'] ?></br>
+    <img src="./images/<?php echo $image[0]['image']; ?>">
+    <hr>
+    <h4>Название: <?= $image[0]['tittle'] ?></h4>
+    Дата добавления: <?= $image[0]['date'] ?></br>
     <?php
-    $img = getimagesize('./images/' . $image['image']);
+    $img = getimagesize(ROOT . '/images/' . $image[0]['image']);
     echo('Размер изображения: ' . $img[0] . ' x ' . $img[1] . ' пикселей.</br>');
-    $mb = filesize('./images/' . $image['image']);
+    $mb = filesize('./images/' . $image[0]['image']);
     $mb = $mb / 1048576;
     echo('Размер файла: ' . round($mb, 2) . ' Мб.');
     ?>
 </div>
-<a href="./index.php"><h4>Вернуться в галерею</h4></a>
+<h4><a href="./index.php">Вернуться в галерею</a></h4>
