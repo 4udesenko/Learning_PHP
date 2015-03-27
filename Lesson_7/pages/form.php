6f1ddcb5d559f58b2acd20a2eeb806ff424f4445
4udesenko <nikita@joylia.com>
2015-03-27 16:21:01 +0300

    Update form.php

--- Lesson_7/pages/form.php
+++ Lesson_7/pages/form.php
@@ -1,29 +1,8 @@
 <?php
-//Добавление изображений в базу
-function add_to_gallery()
-{
-    $img = $_FILES['image']['name'];
-    $tittle = $_POST['tittle'];
-    db_connect();
-    $query = "INSERT INTO images (tittle, image, date) VALUES ('$tittle', '$img', CURRENT_DATE)";
-    $result = mysql_query($query);
-    return $result;
-}
-
-function excess($files)
-{
-    $result = [];
-    foreach ($files as $item) {
-        if ($files[$item] != "." && $files[$item] != "..") {
-            $result[] = $files[$item];
-        }
-    }
-    return $result;
-}
 
 $dir = ROOT . '/images';
 $files = scandir($dir);
-$files = excess($files);
+$files = process($files);
 
 $newName = basename($_FILES['image']['name']);
 
@@ -38,9 +17,10 @@ if (is_uploaded_file($_FILES['image']['tmp_name'])) {
             $_FILES['image']['tmp_name'],
             "$dir/$newName"
         );
-        add_to_gallery();
+        add_to_gallery($_POST['tittle'], $_FILES['image']['name']);
         echo 'Изображение добавлено';
         echo('<p><a href="index.php?view=form">Вернуться к форме</a></p>');
+        echo('<p><a href="index.php?view=images">Вернуться в галерею</a></p>');
         exit;
     }
 }
